a7dd32f06aebe8c0d96580bc3848733443d917f6
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2020-06-04 16:21:07 +0530

    authenticated

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,9 +1,11 @@
-// this is to combine both the resolvers
-const postsResolvers = require('./post');
-const usersResolvers = require('./users')
+const postsResolvers = require('./posts');
+const usersResolvers = require('./users');
 
 module.exports = {
-    Query: {
-        ...postsResolvers.Query // Spread Operator
-    }
-}
+  Query: {
+    ...postsResolvers.Query
+  },
+  Mutation: {
+    ...usersResolvers.Mutation
+  }
+};
--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const { ApolloServer } = require('apollo-server');
-const mongoose = require('mongoose') // mongoose is orm library
+const mongoose = require('mongoose');
 
-const typeDefs = require('./graphql/typedefs')
+const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 const { MONGODB } = require('./config.js');
 
-
-// setup apollo server : running express behind the scenes
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+  typeDefs,
+  resolvers
 });
 
-mongoose.connect(MONGODB, { useNewUrlParser: true})
-    .then(() => {
-        console.log('MongoDB Connected')
-        return server.listen({port: 5000})
-    })
-    .then(res => {
-        console.log(`Server running at ${res.url}`)
-    })
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log('MongoDB Connected');
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
